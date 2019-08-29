8d37e281687b4badf28e0767aa1778ce935330da
lion <lion2018666@gmail.com>
2019-08-29 19:47:55 +0800

    添加样式

--- src/models/dbmodels.go
+++ src/models/dbmodels.go
@@ -1,22 +1,27 @@
 package models
-import (
 
+import (
+	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql" // import your used driver
 	"time"
 )
+
 // Model Struct
 type User struct {
-	Id   int
-	Name string `orm:"size(100)"`
+	Id         int
+	Name       string `orm:"size(100)"`
 	CreateTime time.Time
+	Sort       int
 }
 
 func init() {
+
+	var conn = beego.AppConfig.String("conn")
 	// set default database
 	maxIdle := 30
 	maxConn := 30
-	orm.RegisterDataBase("default", "mysql", "root:!QAZxsw2@tcp(10.10.15.154:3306)/pfxt?charset=utf8", maxIdle, maxConn)
+	orm.RegisterDataBase("default", "mysql", conn, maxIdle, maxConn)
 
 	// register model
 	orm.RegisterModel(new(User))
